5de16fa3982e495a29b7ef15300a947d2192518a
ElusivePenguin <63329346+ElusivePenguin@users.noreply.github.com>
2020-04-26 14:20:54 -0700

    Update userGUI.js

--- js/userGUI.js
+++ js/userGUI.js
@@ -3,7 +3,7 @@ firebase.database().ref("Users").on("value", function(snapshot){
   uName = snapshot.val().userName;
 
   var combine = "Welcome  " + uName;
-  var changeText = combine.fontsize(7).bold().fontcolor("red");
+  var changeText = combine.fontsize(7).bold().fontcolor("#d4870a");
   var welcomeBanner = changeText;
   document.getElementById("usersName").innerHTML = welcomeBanner;
 })
